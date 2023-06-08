8d5576c221e542b6a7b00f594e66ad48fcab6fb3
Melaine Kanana <melaine.kanana@student.moringaschool.com>
2023-06-08 08:17:38 +0300

    completed assignment

--- lib/testing_env/module_test.py
+++ lib/testing_env/module_test.py
@@ -2,17 +2,14 @@ from versions import (
     python_version,
     requests_version,
     pytest_version
-)
-
+) 
 def test_python_version():
     version_info = python_version()
     assert version_info.major == 3
-    assert version_info.minor == 8
+    assert version_info.minor == 9
 
-
-def test_requests_version():
+def test_request_version():
     assert requests_version() == "2.27.1"
 
-
 def test_pytest_version():
-    assert pytest_version() == "7.1.3"
+    assert pytest_version() == "6.2.5"        
